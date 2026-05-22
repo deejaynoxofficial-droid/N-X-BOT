1369a65ed526c8acf66a71ee0ababdfd4afa4d13
Noxstar256 <deejaynoxofficial@gmail.com>
2026-05-22 06:33:02 +0300

    Delete alive.js

--- Commands/alive.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-name: 'alive',
-async execute(sock,m,args,{ BOTNAME }) {
-const from = m.key.remoteJid
-
-await sock.sendMessage(from,{ text:`${BOTNAME} is active and running successfully` })
-}
-}
