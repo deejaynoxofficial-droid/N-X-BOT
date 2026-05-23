c8ba1d2ff90e037cf13aee215ea091cfd4e955c3
Noxstar256 <deejaynoxofficial@gmail.com>
2026-05-23 14:41:40 +0300

    Update welcome.js

--- listeners/welcome.js
+++ listeners/welcome.js
@@ -342,5 +342,13 @@ module.exports = {
 
             } catch {}
         }
+    },
+
+    async run(sock, msg, args) {
+        return this.execute(
+            sock,
+            msg,
+            args
+        )
     }
 }
