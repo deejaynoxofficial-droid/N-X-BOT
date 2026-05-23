9e5939f5e10e8dae42f51ef68264bea202d38dbf
Noxstar256 <deejaynoxofficial@gmail.com>
2026-05-23 12:01:19 +0300

    Update functions.js

--- utils/functions.js
+++ utils/functions.js
@@ -103,7 +103,8 @@ function isUrl(url) {
         const regex =
             /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi
 
-        return regex.test(url)
+        return Boolean(
+            url.match(regex))
 
     } catch {
 
