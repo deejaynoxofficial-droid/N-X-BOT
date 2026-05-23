9819a1f0ce1491da4000687ec53e7c2346fb1cae
Noxstar256 <deejaynoxofficial@gmail.com>
2026-05-23 14:34:40 +0300

    Update autoread.js

--- listeners/autoread.js
+++ listeners/autoread.js
@@ -235,5 +235,13 @@ module.exports = {
 
             } catch {}
         }
+    },
+
+    async run(sock, msg, args) {
+        return this.execute(
+            sock,
+            msg,
+            args
+        )
     }
 }
