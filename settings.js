11e8964506f5016ced6ed30abd3208e968aebd42
Noxstar256 <deejaynoxofficial@gmail.com>
2026-05-24 11:43:11 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -227,7 +227,21 @@ module.exports = {
             'https://api.openai.com',
 
         gemini:
-            'https://generativelanguage.googleapis.com'
+            'curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent" \
+  -H 'Content-Type: application/json' \
+  -H 'X-goog-api-key: AIzaSyDN8XiRkh0vZtDVVZgYOaiIZXmI1zbMYdk' \
+  -X POST \
+  -d '{
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": "Explain how AI works in a few words"
+          }
+        ]
+      }
+    ]
+  }'respond normal'
     },
 
     //========================================
