ec4d74002371cd2c56768ccc5fc421d05757376d
Noxstar256 <deejaynoxofficial@gmail.com>
2026-05-25 09:28:58 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,57 +1,19 @@
-const { runtime } = require('../utils/functions')
-
 module.exports = {
+
     name: 'ping',
+
+    aliases: ['test'],
 
     async execute(sock, msg) {
 
-        const from = msg.key.remoteJid
-
-        try {
-
-            const start = process.hrtime()
-
-            await sock.sendMessage(from, {
-                text: '🏓 Checking bot speed...'
-            })
-
-            const end = process.hrtime(start)
-
-            const speed =
-                (
-                    end[0] * 1000 +
-                    end[1] / 1000000
-                ).toFixed(2)
-
-            const text = `
-╭━━━〔 🏓 PING STATUS 〕━━━⬣
-┃
-┃ ⚡ Speed: ${speed} ms
-┃ 🕒 Runtime: ${runtime(process.uptime())}
-┃ 🤖 Status: Online
-┃ 🚀 Performance: Stable
-┃
-╰━━━━━━━━━━━━━━━━━━⬣
-`
-
-            await sock.sendMessage(from, {
-                text
-            })
-
-        } catch (error) {
-
-            console.log(
-                'Ping Error:',
-                error
-            )
-
-            try {
-
-                await sock.sendMessage(from, {
-                    text: '❌ Failed to check ping.'
-                })
-
-            } catch {}
-        }
+        await sock.sendMessage(
+            msg.key.remoteJid,
+            {
+                text: '🏓 Pong working'
+            },
+            {
+                quoted: msg
+            }
+        )
     }
 }
